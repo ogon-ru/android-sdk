87d736bc19ead66d10d949e0fa4927606bfc7e97
Egor Komarov <e.komarov@pnhub.ru>
2021-02-05 19:23:47 +0300

    ECOSYSTEM-1059 back button fix

--- sdktest/src/main/java/ru/pnhub/sdktest/MainActivity.kt
+++ sdktest/src/main/java/ru/pnhub/sdktest/MainActivity.kt
@@ -12,12 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJHUEIgRWNvc3lzdGVtIiwidWlkIjoxLCJpc3MiOiJHUEIiLCJleHAiOjE2MTA3NDQ0MDAsImlhdCI6MTYwMjE1NzQzMiwic3RwIjoxLCJ1Z3AiOjF9.AD39LqK90armvlffvZQrm5WPSIWPWLdpjnsTX5oUGRN3LckP9Esg9ZUgCeeMRYpkPDUu-1nVw0G0XGN92g8h3wYGVAYEiLsbWQIwPafHNEZGEXL5jKXm-yuPTBKS5cQCak3PIHDb-eH9zGZ8FCgtGRN0MnS-CqlsWzV55qG-WMvJJDaAefcL6CH1QcX_9LRBrJFLR7He7RA6Ju6e-nX6_5p2yO0hN4QHb-2SsRFMMsxa4GZGP63Tlp3N7LZQDefmieQHa8T_tn_uf2DiVUHp9-YRwsxC1l6f-y6rCe2l557KoSsLxVK5xTCa5MLaFnfCcfy_oShoB3JLdY8Ev3O-lg"
+        val token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJHUEIgRWNvc3lzdGVtIiwiZXhwIjo0NzY1NDI2NjcwLCJpYXQiOjE2MTE4MjY2NzAsImlzcyI6IkdQQiIsInVpZCI6MzI0LCJ1Z3AiOjEsInN0cCI6MH0.UTot-Qqfc2-jqF6D436fIt7Idt0x6-xHw8BGjSVOaI2SMzafn5HhHOCXaIdzkTIcvj---G7JtUL5awbYQuncLGVCYHDLoEZhpdz9-4PqrlLi839x4aK_Cg74OuZuuYKzffe7exGLcQqi-OsJqIIEVh3BBFaB_F2opSi43wdpj0r9X767H0WHpvUUpIKpsLIpc7_nm-UrnET3pmRHfx128SGlqvk8X7_vE1Hfo439SgtHX-uBaJt8cmUQNnl-DYaAcSt0K4IrDRPTpYSbocxe9vO5c9kGabKrQp9a73L5irGtPDvigRQ6Do99rwY_ZAhO0iXUuDdYb4x0TkS2VI717A"
 
         findViewById<Button>(R.id.button).apply {
             setOnClickListener {
                 val intent = Intent(this@MainActivity, WidgetActivity::class.java).apply {
                     putExtra(WidgetActivity.EXTRA_TOKEN, token)
+                    putExtra(WidgetActivity.EXTRA_BASE_URL, "https://widget.setpartnerstv.com")
 //                    putExtra(WidgetActivity.EXTRA_BASE_URL, "https://192.168.31.80:4305")
                 }
 
